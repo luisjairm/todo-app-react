a34df9af303bf1a3104b8997b2acb734f8fae0c1
luisjairm <luisjairm.dev@gmail.com>
2023-03-19 11:43:06 -0600

    config tailwind

--- main.jsx
+++ main.jsx
@@ -1,3 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import App from './src/App'
+import './index.css'
+
 const root = createRoot(document.getElementById('app'))
-root.render(<h1>Hola mundo</h1>)
+root.render(<App />)
