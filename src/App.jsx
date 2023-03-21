574af9a07b82f323ff03761185a1af16f6cea305
luisjairm <luisjairm.dev@gmail.com>
2023-03-20 21:47:27 -0600

    tareas

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ListTasks from './components/ListTasks'
 const App = () => {
   const [tasks, setTasks] = useState([])
   const [completedTasks, setCompletedTasks] = useState([])
+  const [taskEditing, setTaskEditing] = useState({})
 
   useEffect(() => {
     console.log('tasks ', tasks)
@@ -24,35 +25,38 @@ const App = () => {
     const tmpTasks = tasks.filter(task => task.id !== id)
     setTasks(tmpTasks)
   }
+  const removeTaskCompleted = (id) => {
+    const tmpTasksCompleted = completedTasks.filter(task => task.id !== id)
+    setCompletedTasks(tmpTasksCompleted)
+  }
 
   return (
     <div
       className='container mx-auto mt-20'
     >
-      <h1
-        className='text-center font-bold text-4xl'
-      >Todo App
+      <h1 className='text-center font-bold text-4xl'>Todo App
       </h1>
-      <div
-        className='md:flex mt-12'
-      >
-        <div
-          className='md:w-1/2 m-5'
-        >
+      <div className='md:flex mt-12 flex-col'>
+        <div className='w-full md:flex'>
           <Form
             tasks={tasks}
             setTasks={setTasks}
+            taskEditing={taskEditing}
+            setTaskEditing={setTaskEditing}
           />
-          <ListCompletedTasks
-            completedTasks={completedTasks}
-            setCompletedTasks={setCompletedTasks}
+          <ListTasks
+            tasks={tasks}
+            setTasks={setTasks}
+            completeTask={completeTask}
+            removeTask={removeTask}
+            setTaskEditing={setTaskEditing}
           />
         </div>
-        <ListTasks
-          tasks={tasks}
-          setTasks={setTasks}
-          completeTask={completeTask}
-          removeTask={removeTask}
+
+        <ListCompletedTasks
+          completedTasks={completedTasks}
+          setCompletedTasks={setCompletedTasks}
+          removeTaskCompleted={removeTaskCompleted}
         />
       </div>
     </div>
