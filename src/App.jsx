d7e369af57f4b41dcfe47a3eeb4eea342a6903c2
luisjairm <luisjairm.dev@gmail.com>
2023-03-20 21:54:14 -0600

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,29 @@ const App = () => {
   const [taskEditing, setTaskEditing] = useState({})
 
   useEffect(() => {
-    console.log('tasks ', tasks)
-    console.log('completed tasks ', completedTasks)
+    const obtenerLS = () => {
+      // eslint-disable-next-line no-undef
+      const tasksLocal = JSON.parse(localStorage.getItem('tasks')) ?? []
+      setTasks(tasksLocal)
+
+      // eslint-disable-next-line no-undef
+      const completedTasksLocal = JSON.parse(localStorage.getItem('completedTasks')) ?? []
+      setCompletedTasks(completedTasksLocal)
+    }
+    obtenerLS()
+  }, [])
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    localStorage.setItem('tasks', JSON.stringify(tasks))
   }, [tasks])
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    localStorage.setItem('completedTasks', JSON.stringify(completedTasks))
+  }, [completedTasks])
 
   const completeTask = (id) => {
     const tmpTasks = tasks.filter(task => task.id !== id)
     const tmpCompleteTask = tasks.filter(task => task.id === id)
-
     setTasks(tmpTasks)
     setCompletedTasks([...completedTasks, tmpCompleteTask[0]])
   }
