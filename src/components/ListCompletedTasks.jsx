298010464eaed6113f51daf8ab7671f1d0ab6193
luisjairm <luisjairm.dev@gmail.com>
2023-03-20 21:46:31 -0600

    eliminar tareas

--- src/components/ListCompletedTasks.jsx
+++ src/components/ListCompletedTasks.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import ItemCompletedTask from './ItemCompletedTask'
 
-const ListCompletedTasks = ({ completedTasks }) => {
-  console.log(JSON.stringify(completedTasks, null, 2))
+const ListCompletedTasks = ({ completedTasks, removeTaskCompleted }) => {
   return (
-    <div className='w-full'>
+    <div className='md:w-1/2 m-5 mb-10'>
       <h1 className='font-black text-center text-xl my-5'>Tareas Completadas</h1>
-      <div>
-        {
+      {
+        completedTasks.length > 0
+          ? (
+            <div>
+              {
                 completedTasks.map(task => (
                   <ItemCompletedTask
                     key={task.id}
                     task={task}
+                    removeTaskCompleted={removeTaskCompleted}
                   />
                 ))
             }
-      </div>
+            </div>
+            )
+          : (
+            <div className='bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4' role='alert'>
+              <p className='font-bold'>Vacio</p>
+              <p>Aun no has marcado ninguna tarea como completada</p>
+            </div>
+            )
+      }
     </div>
   )
 }
