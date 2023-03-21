c8469cb9d53927f0a9004ae74cec73e99345f402
luisjairm <luisjairm.dev@gmail.com>
2023-03-20 18:11:08 -0600

    funciones: eliminar tareas completar tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import Form from './components/Form'
+import ListCompletedTasks from './components/ListCompletedTasks'
+import ListTasks from './components/ListTasks'
 
-function App () {
+const App = () => {
+  const [tasks, setTasks] = useState([])
+  const [completedTasks, setCompletedTasks] = useState([])
+
+  useEffect(() => {
+    console.log('tasks ', tasks)
+    console.log('completed tasks ', completedTasks)
+  }, [tasks])
+
+  const completeTask = (id) => {
+    const tmpTasks = tasks.filter(task => task.id !== id)
+    const tmpCompleteTask = tasks.filter(task => task.id === id)
+
+    setTasks(tmpTasks)
+    setCompletedTasks([...completedTasks, tmpCompleteTask[0]])
+  }
+
+  const removeTask = (id) => {
+    const tmpTasks = tasks.filter(task => task.id !== id)
+    setTasks(tmpTasks)
+  }
+
   return (
-    <h1>App</h1>
+    <div
+      className='container mx-auto mt-20'
+    >
+      <h1
+        className='text-center font-bold text-4xl'
+      >Todo App
+      </h1>
+      <div
+        className='md:flex mt-12'
+      >
+        <div
+          className='md:w-1/2 m-5'
+        >
+          <Form
+            tasks={tasks}
+            setTasks={setTasks}
+          />
+          <ListCompletedTasks
+            completedTasks={completedTasks}
+            setCompletedTasks={setCompletedTasks}
+          />
+        </div>
+        <ListTasks
+          tasks={tasks}
+          setTasks={setTasks}
+          completeTask={completeTask}
+          removeTask={removeTask}
+        />
+      </div>
+    </div>
   )
 }
 
