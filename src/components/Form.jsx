709f17762cc5f1410fdfec677060b884fd77c630
luisjairm <luisjairm.dev@gmail.com>
2023-03-20 21:46:19 -0600

    actualizar tareas

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Error from './Error'
 
-const Form = ({ tasks, setTasks }) => {
+const Form = ({ tasks, setTasks, taskEditing, setTaskEditing }) => {
   const [taskName, setTaskName] = useState('')
   const [taskPriority, setTaskPriority] = useState('alta')
   const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if (Object.keys(taskEditing).length > 0) {
+      setTaskName(taskEditing.name)
+      setTaskPriority(taskEditing.priority)
+    }
+  }, [taskEditing])
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -17,18 +24,35 @@ const Form = ({ tasks, setTasks }) => {
       return
     }
 
-    const tmpTask = {
-      id: Date.now(),
-      name: taskName,
-      priority: taskPriority
+    if (taskEditing.id) {
+      const tmpTasks = tasks.map(task => {
+        if (task.id === taskEditing.id) {
+          return {
+            id: taskEditing.id,
+            name: taskName,
+            priority: taskPriority
+          }
+        }
+        return task
+      })
+      setTasks(tmpTasks)
+      setTaskEditing({})
+    } else {
+      const tmpTask = {
+        id: Date.now(),
+        name: taskName,
+        priority: taskPriority
+      }
+
+      setTasks([...tasks, tmpTask])
     }
 
-    setTasks([...tasks, tmpTask])
     setTaskName('')
+    setTaskPriority('alta')
   }
 
   return (
-    <div className='w-full rounded-md'>
+    <div className='md:w-1/2 rounded-md m-5'>
       <h1 className='font-black text-center text-xl mb-5'>Agrega tus tareas</h1>
       <form
         className='bg-white shadow-md p-5 rounded-md'
@@ -61,6 +85,7 @@ const Form = ({ tasks, setTasks }) => {
             name='priority'
             id='priority'
             onChange={(e) => setTaskPriority(e.target.value)}
+            value={taskPriority}
           >
             <option value='alta'>Alta</option>
             <option value='media'>Media</option>
@@ -69,7 +94,7 @@ const Form = ({ tasks, setTasks }) => {
         </div>
         <input
           type='submit'
-          value='Agregar Tarea'
+          value={taskEditing.id ? 'Actualizar Tarea' : 'Agregar Tarea'}
           className='bg-green-500 hover:bg-green-700 rounded px-5 py-1 text-white text-xl mt-5 '
         />
       </form>
